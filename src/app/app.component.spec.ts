7c7fa2b24a5fc90612b750b38e7b1caebc86cc7f
MirianQuispe <quispemiriamaldana@gmail.com>
2022-09-12 15:54:03 -0300

    incorporando cambios en componente principal

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'app-maquinaria'`, () => {
+  it(`should have as title 'app-maquimax'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('app-maquinaria');
+    expect(app.title).toEqual('app-maquimax');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('app-maquinaria app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('app-maquimax app is running!');
   });
 });
